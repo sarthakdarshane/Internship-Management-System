2b4e8526ac6ef1cf6330929b9c57bdb43e683c8a
sarthakdarshane <sarthakdarshane2018@gmail.com>
2026-08-21 21:21:56 +0530

    Initial project setup

---
+++ Backend/config/database.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user:process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+
+pool.on("connect", ()=>{
+    console.log("Postgress Conncected");
+});
+
+pool.on("error", (err)=>{
+    console.log("Postgress Connection error:",err);
+});
+
+module.exports = pool;
+
